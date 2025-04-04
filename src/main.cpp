5be628726308fc0161e6f9ff63f3ec1192e87ab1
apple-39 <r123222@g.kobe-kosen.ac.jp>
2025-04-04 18:28:11 +0900

    エンコーダの値が読めることを確認した。

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+#include <Encoder.h>
+
+Encoder enc_x(21 , 20);
+Encoder enc_y(39 , 38);
 
 void setup()
 {
+    Serial.begin(115200);
 }
 
 void loop()
 {
+    Serial.print(" x座標 : ");
+    Serial.print(enc_x.read());
+    Serial.print(" y座標 : ");
+    Serial.println(enc_y.read());
 }
