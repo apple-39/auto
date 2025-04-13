8082a77e3ba45f7f96f618618b5599cf48a413e4
apple-39 <r123222@g.kobe-kosen.ac.jp>
2025-04-13 16:41:20 +0900

    fix/やっぱりswitchを抜け出せていなかったので修正した。コンストラクタがやっぱりわからない。

--- src/input_target.cpp
+++ src/input_target.cpp
@@ -12,16 +12,32 @@ void  input_target::set_position(){
     switch (phase)
     {
     case 1:
-        end.cal.input_calc(100 , 0 , 0);
+    end.cal.input_calc(
+        target_config.phase1_x,
+        target_config.phase1_y,
+        target_config.phase1_z);
+    break;
 
     case 2:
-        end.cal.input_calc(100 , 100 , 0);
+    end.cal.input_calc(
+        target_config.phase2_x,
+        target_config.phase2_y,
+        target_config.phase2_z);
+    break;
 
     case 3:
-        end.cal.input_calc(0 , 100 , 0);    
+    end.cal.input_calc(
+        target_config.phase3_x,
+        target_config.phase3_y,
+        target_config.phase3_z);
+    break;   
     
     case 4:
-        end.cal.input_calc(0 , 0 , 0);
+    end.cal.input_calc(
+        target_config.phase4_x,
+        target_config.phase4_y,
+        target_config.phase4_z);
+    break;
         
     default:
         break;
